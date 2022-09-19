519fdfa143dd1b5b2956b81e38042ca8b45e45ad
BRYAN RASSWEILER <bryan_rassweiler@estudante.sc.senai.br>
2022-09-19 12:32:50 -0300

    .

--- src/main/java/com/battipapo/controller/IndexController.java
+++ src/main/java/com/battipapo/controller/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
 }
